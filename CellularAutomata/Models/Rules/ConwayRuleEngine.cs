d3f6c7a668fc0dbe3a859c43bbc33173528fe8bd
Skye Doto <sdoto@northwell.edu>
2020-12-20 11:30:35 -0500

    Add instructions for rule engines

--- CellularAutomata/Models/Rules/ConwayRuleEngine.cs
+++ CellularAutomata/Models/Rules/ConwayRuleEngine.cs
@@ -5,6 +5,15 @@ namespace CellularAutomata.Models.Rules
 {
     public class ConwayRuleEngine : IRuleEngine
     {
+        /// <summary>
+        /// given a cell state and the state of the neighbors of the cell in the form
+        ///
+        /// [0, 1, 2,
+        ///  3,    4,
+        ///  5, 6, 7]
+        ///
+        /// Return the new state of the cell
+        /// </summary>
         public override bool ApplyRule(bool cell, List<bool?> neighbors)
         { 
             int livingNeighbors = neighbors.Count(x => x.HasValue && x.Value);
